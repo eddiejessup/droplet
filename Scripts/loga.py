279fccd37e98e79a9353c20ff0b1b0f2ad5a8e1d
Elliot Marsden <elliot.marsden@gmail.com>
2012-12-19 13:48:19 +0000

    Adding option to specify colors

--- Scripts/loga.py
+++ Scripts/loga.py
@@ -34,10 +34,10 @@ def suffix_remove(s, suffix):
     if s.endswith(suffix): return s[:-len(suffix)]
     else: return s
 
-def plot(r, label=None):
+def plot(r, label=None, color=None):
     rs = smooth(r['sense'], 4)
     rd = smooth(r['dvar'], 4)
-    pp.plot(rs, rd, lw=0.8, label=label)
+    pp.plot(rs, rd, lw=0.8, label=label, c=color)
 
 def parse(fname):
     n = np.loadtxt(fname, skiprows=1).shape[1]
@@ -60,37 +60,48 @@ parser.add_argument('-o', '--out', default='out', nargs='?',
     help='filename of the output image')
 parser.add_argument('-l', '--labels', default=[], nargs='*',
     help='labels for each log')
+parser.add_argument('-c', '--colors', default=[], nargs='*',
+    help='colors for each log')
+parser.add_argument('-x', '--xlabel', default=None, nargs='?',
+    help='xlabel')
 parser.add_argument('-s', '--silent', default=False, action='store_true',
     help='don\'t show plot')
 args = parser.parse_args()
 
 if args.labels and len(args.labels) != len(args.dirs) + len(args.files): raise Exception
+if args.colors and len(args.colors) != len(args.dirs) + len(args.files): raise Exception
 args.out = suffix_remove(args.out, '.png')
 args.out = suffix_remove(args.out, '.dat')
 
+i = 0
 for dir_name in args.dirs:
     dir_name = suffix_remove(dir_name, '/')
     fname = '%s/log.dat' % dir_name
     r = parse(fname)
-    if args.mode == 'p': plot(r, label=dir_name)
+    if args.mode == 'p': plot(r, label=dir_name, color=args.colors[i])
     elif args.mode == 'a':
         try:
             r_dvar_sum += r['dvar']
         except NameError:
             r_dvar_sum = r['dvar']
+    i += 1
 
 for fname in args.files:
     r = parse(fname)
     if args.mode == 'p': 
-        plot(r, label=fname)
+        plot(r, label=fname, color=args.colors[i])
     elif args.mode == 'a':
         try:
             r_dvar_sum += r['dvar']
         except NameError:
             r_dvar_sum = r['dvar']
+    i += 1
 
 if args.mode == 'p':
-    pp.xlabel(r'$\chi$', size=20)
+    if not args.xlabel:
+        pp.xlabel(r'$\chi$', size=20, labelpad=10)
+    else:
+        pp.xlabel(r'$%s$' % args.xlabel, size=20, labelpad=10   )
     pp.ylabel(r'$\sigma$', size=20)
     if args.labels:
         pp.legend(args.labels, loc='lower right')
