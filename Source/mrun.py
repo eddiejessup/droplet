8db37e021ac4d810044abfb255bf498d2bf58133
Elliot Marsden <elliot.marsden@gmail.com>
2013-01-08 16:56:42 +0000

    Fiddling with run script

--- Source/mrun.py
+++ Source/mrun.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
 import argparse
 import shutil
 import yaml
@@ -11,15 +12,17 @@ parser.add_argument('f',
     help='YAML file containing system parameters')
 parser.add_argument('-d', '--dir', default=None,
     help='output directory, default is no output')
-parser.add_argument('-e', '--every', default=1,
+parser.add_argument('-e', '--every', type=int, default=1,
     help='how many iterations should elapse between outputs, default is 1')
 parser.add_argument('-t', '--runtime', type=float, default=float('inf'),
     help='how long to run, default is forever')
 parser.add_argument('-p', '--plot', default=False, action='store_true',
     help='plot system directly, default is false')
+parser.add_argument('-s', '--silent', default=False, action='store_true',
+    help='don''t print to stdout')
 args = parser.parse_args()
 
-print('Initialising...')
+if not args.silent: print('Initialising...')
 
 system = System.System(yaml.safe_load(open(args.f, 'r')))
 
@@ -29,15 +32,20 @@ if args.dir is not None:
     utils.makedirs_soft('%s/Persistent' % args.dir)
     system.output_persistent('%s/Persistent/' % args.dir)
 
-print('Initialisation done! Starting...')
+if not args.silent: print('Initialisation done! Starting...')
 
 while system.t < args.runtime:
     system.iterate()
-
-    if args.dir is not None and not system.i % args.every:
-        state_dirname = '%s/%f' % (args.dir, system.t)
-        utils.makedirs_soft(state_dirname)
-        system.output(state_dirname)
-        if args.plot: system.plot(state_dirname)
+    if not system.i % args.every:
+        if not args.silent:
+            print('t:%010g i:%08i' % (system.t, system.i), end=' ')
+        if args.dir is not None:
+            if not args.silent: print('making output...', end='')
+            state_dirname = '%s/%f' % (args.dir, system.t)
+            utils.makedirs_soft(state_dirname)
+            system.output(state_dirname)
+            if args.plot: system.plot(state_dirname)
+            print('finished', end='')
+        print()
 
 print('Finished!')
