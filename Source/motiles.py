bfbcde53b3fe899ee029b49871db4e9b4428c493
Elliot Marsden <elliot.marsden@gmail.com>
2013-01-14 11:10:19 +0000

    Improving correctness checking of motile velocities and rotational diffusion

--- Source/motiles.py
+++ Source/motiles.py
@@ -5,7 +5,8 @@ import cell_list
 import tumble_rates as tumble_rates_module
 import motile_numerics
 
-v_TOLERANCE = 1e-15
+v_TOLERANCE = 1e-12
+D_rot_tolerance = 1.0
 
 class Motiles(object):
     def __init__(self, parent_env, N, v_0, o, tumble_flag=False, tumble_args=None,
@@ -33,7 +34,6 @@ class Motiles(object):
         self.rot_diff_flag = rot_diff_flag
         if self.rot_diff_flag:
             self.D_rot = rot_diff_args['D_rot']
-
             if self.D_rot < 0.0:
                 raise Exception('Require rotational diffusion constant >= 0')
 
@@ -51,19 +51,21 @@ class Motiles(object):
         self.v = utils.point_pick_cart(self.parent_env.dim, self.N) * self.v_0
 
     def initialise_r(self, o):
-        self.r = np.random.uniform(-self.parent_env.L_half, self.parent_env.L_half,
-            size=(self.N, self.parent_env.dim))
-        for r in self.r:
-            while o.is_obstructed(r):
-                r = np.random.uniform(-self.parent_env.L_half,
-                    self.parent_env.L_half, self.parent_env.dim)
+        self.r = np.zeros([self.N, self.parent_env.dim], dtype=np.float)
+        for i in range(self.N):
+            while True:
+                self.r[i] = np.random.uniform(-self.parent_env.L_half, self.parent_env.L_half, self.parent_env.dim)
+                if not o.is_obstructed(self.r[i]): break
+
+    def check_v(self):
+        ''' Check motile speeds are all v_0 to within a tolerance '''
+        assert np.abs(utils.vector_mag(self.v) / self.v_0 - 1.0).max() < v_TOLERANCE
 
     def iterate(self, c, o):
         if self.vicsek_flag: self.vicsek()
         if self.tumble_flag: self.tumble(c)
         if self.force_flag: self.force(c)
         if self.rot_diff_flag: self.rot_diff()
-        assert abs(utils.vector_mag(self.v).mean() / self.v_0 - 1.0) < v_TOLERANCE
         o.obstruct(self)
 
     def tumble(self, c):
@@ -71,7 +73,7 @@ class Motiles(object):
         v_mags = utils.vector_mag(self.v[i_tumblers])
         self.v[i_tumblers] = utils.point_pick_cart(self.parent_env.dim, len(i_tumblers))
         self.v[i_tumblers] *= v_mags[:, np.newaxis]
-        assert abs(utils.vector_mag(self.v).mean() / self.v_0 - 1.0) < v_TOLERANCE
+        self.check_v()
 
     def force(self, c):
         v_old_mags = utils.vector_mag(self.v)
@@ -80,43 +82,24 @@ class Motiles(object):
         i_up = np.where(np.sum(self.v * grad_c_i, 1) > 0.0)
         v_new[i_up] += self.force_sense * grad_c_i[i_up] * self.parent_env.dt
         self.v = utils.vector_unit_nullnull(v_new) * v_old_mags[:, np.newaxis]
-        assert abs(utils.vector_mag(self.v).mean() / self.v_0 - 1.0) < v_TOLERANCE
+        self.check_v()
 
     def rot_diff(self):
-        v_before = self.v.copy()
+        v_initial = self.v.copy()
         if self.parent_env.dim == 2:
             self.v = utils.rot_diff_2d(self.v, self.D_rot, self.parent_env.dt)
         elif self.parent_env.dim == 3:
             self.v = utils.rot_diff_3d(self.v, self.D_rot, self.parent_env.dt)
         else: raise Exception('Rotational diffusion not implemented in this '
             'dimension')
-        dtheta = utils.vector_angle(v_before, self.v)
-        dtheta_var = (dtheta ** 2).sum() / (len(dtheta) - 1)
-        D_rot_calc = dtheta_var / (2.0 * self.parent_env.dt)
-        D_rot_error = 1.0 - D_rot_calc / self.D_rot
-        assert abs(D_rot_error) < 10.0, D_rot_error
-#        print('D_rot_error: %f %%' % (100.0 * D_rot_error))
-        assert abs(utils.vector_mag(self.v).mean() / self.v_0 - 1.0) < v_TOLERANCE
+        self.check_v()
+        D_rot_calc = utils.calc_D_rot(v_initial, self.v, self.parent_env.dt)
+        assert abs((D_rot_calc / self.D_rot) - 1.0) < D_rot_tolerance
 
     def vicsek(self):
         inters, intersi = cell_list.interacts(self.r, self.parent_env.L, self.vicsek_R)
         self.v = motile_numerics.vicsek_inters(self.v, inters, intersi)
-        assert abs(utils.vector_mag(self.v).mean() / self.v_0 - 1.0) < v_TOLERANCE
+        self.check_v()
 
     def get_density_field(self, dx):
         return fields.density(self.r, self.parent_env.L, dx)
-
-    def output(self, dirname, prefix=''):
-        np.save('%s/%sr' % (dirname, prefix), self.r)
-        np.save('%s/%sv' % (dirname, prefix), self.v)
-        if self.tumble_flag: self.tumble_rates.output(dirname, prefix=prefix+'tr_')
-
-    def output_persistent(self, dirname, prefix=''):
-        file = open('%s/%sparams.dat' % (dirname, prefix), 'w')
-        file.write('N,%i\n' % self.N)
-        file.write('v_0,%f\n' % self.v_0)
-        if self.force_flag: file.write('force_sense,%f\n' % self.force_sense)
-        if self.rot_diff_flag: file.write('D_rot,%f\n' % self.D_rot)
-        if self.vicsek_flag: file.write('vicsek_R,%f\n' % self.vicsek_R)
-        file.close()
-        if self.tumble_flag: self.tumble_rates.output_persistent(dirname, prefix=prefix+'tr_')
