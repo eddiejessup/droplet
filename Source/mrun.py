29a03ffa40eb8df17fb7629c57fb259046490650
Elliot Marsden <elliot.marsden@gmail.com>
2013-01-24 12:56:51 +0000

    Using csv module for log file

--- Source/mrun.py
+++ Source/mrun.py
@@ -11,6 +11,7 @@ from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import utils
 import System
+import csv
 
 parser = argparse.ArgumentParser(description='Run a motile system simulation')
 parser.add_argument('f',
@@ -23,11 +24,19 @@ parser.add_argument('-e', '--every', type=int, default=1,
     help='how many iterations should elapse between outputs, default is 1')
 parser.add_argument('-p', '--plot', default=False, action='store_true',
     help='plot system directly, default is false')
+parser.add_argument('-r', '--positions', default=False, action='store_true',
+    help='output motile positions, default is false')
 parser.add_argument('-s', '--silent', default=False, action='store_true',
     help='don''t print to stdout')
 parser.add_argument('--profile', default=False, action='store_true',
     help='profile program (implies -s -d None)')
 args = parser.parse_args()
+
+def csv_write(f, delimiter=' ', *args):
+    for i in range(len(args)):
+        f.write(args[i])
+        if i < len(args) - 1: f.write(delimiter)
+    f.write('\n')
 
 def main():
     if not args.silent: print('Initialising...')
@@ -36,11 +45,13 @@ def main():
 
     if args.dir is not None:
         utils.makedirs_safe(args.dir)
-        utils.makedirs_soft('%s/r' % args.dir)
+        f_log = open('%s/log.csv' % (args.dir), 'w')
+        csv_log = csv.writer(f_log, delimiter=' ')
+        csv_log.writerow(['t', 'dstd', 'D'])
+        if args.positions: utils.makedirs_soft('%s/r' % args.dir)
         if args.plot:
             utils.makedirs_soft('%s/plot' % args.dir)
             lims = [-system.L_half, system.L_half]
-
             fig_box = pp.figure()
             if system.dim == 2:
                 ax_box = fig_box.add_subplot(111)
@@ -49,9 +60,6 @@ def main():
                     parts_plot = ax_box.scatter([], [], s=1.0, c='k')
                 if system.attractant_flag:
                     c_plot = ax_box.imshow([[0]], extent=2*[-system.L_half, system.L_half], origin='lower', interpolation='nearest', cmap='Greens')
-                utils.makedirs_soft('%s/dist' % args.dir)
-                fig_dist = pp.figure()
-                ax_dist = fig_dist.add_subplot(111)
             elif system.dim == 3:
                 ax_box = fig.add_subplot(111, projection='3d')
                 if system.motiles_flag:
@@ -64,26 +72,17 @@ def main():
             ax_box.set_xlim(lims)
             ax_box.set_ylim(lims)
         shutil.copy(args.f, args.dir)
-        f = open('%s/log.dat' % (args.dir), 'w')
-        f.write('t dstd')
-        f.write('\n')
     if not args.silent: print('Initialisation done! Starting...')
-    Ds=[]
     while system.t < args.runtime:
         if not system.i % args.every:
             if not args.silent:
                 print('t:%010g i:%08i' % (system.t, system.i), end=' ')
-                if system.t > 20.0:
-                    Ds.append(utils.calc_D(system.m.get_r_unwrapped(), system.m.r_0, system.t))
-                    print(Ds[-1])
 
-        #        print 'drift v: ', np.mean(self.v[:, 0])
             if args.dir is not None:
                 if not args.silent: print('making output...', end='')
-                f.write('%f %f' % (system.t, system.m.get_dstd(system.obstructs, 4.0)))
-                f.write('\n')
-                f.flush()
-                np.save('%s/r/%f' % (args.dir, system.t), system.m.r)
+                csv_log.writerow([system.t, system.m.get_dstd(system.obstructs, 4.0), utils.calc_D(system.m.get_r_unwrapped(), system.m.r_0, system.t)])
+                f_log.flush()
+                if args.positions: np.save('%s/r/%f' % (args.dir, system.t), system.m.r)
                 if args.plot:
                     if system.dim == 2:
                         if system.motiles_flag:
@@ -91,11 +90,6 @@ def main():
                         if system.attractant_flag:
                             c_plot.set_data(np.ma.array(system.c.a.T, mask=system.c.of.T))
                             c_plot.autoscale()
-                        ax_dist.set_xlim(lims)
-                        ax_dist.set_ylim([0.0, 1.0/(20**2)])
-                        ax_dist.hist(system.m.r[:, 0], bins=50, range=lims, normed=True)
-                        fig_dist.savefig('%s/dist/%f.png' % (args.dir, system.t))
-                        ax_dist.cla()
                     elif system.dim == 3:
                         if system.motiles_flag:
                             parts_plot._offsets3d = (system.m.r[:, 0], system.m.r[:, 1], system.m.r[:, 2])
