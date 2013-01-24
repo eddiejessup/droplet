e53e20cc47de25c2a2c572e8db1b07e98624359f
Elliot Marsden <elliot.marsden@gmail.com>
2013-01-24 13:11:59 +0000

    Deleting little pseudocode rubbish

--- Source/obsplan.py
+++
@@ -1,6 +0,0 @@
-filler = np.frompyfunc(lambda x: list(), 1, 1)
-cl = np.empty([M, M], dtype=np.object)
-filler(cl, cl)
-inds = utils.r_to_i(r, dx)
-for i in range(len(inds)):
-    cl[inds[i, 0], inds[i, 1]].append(i)
