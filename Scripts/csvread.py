ab3321e156c3aec2b40eafc6be6330d1517369e6
Elliot Marsden <elliot.marsden@gmail.com>
2013-03-02 17:39:05 +0000

    Making log analysis script work with any field, not just drift speed

--- Scripts/csvread.py
+++ Scripts/csvread.py
@@ -5,17 +5,17 @@ import matplotlib.mlab as mlb
 
 fname = sys.argv[1]
 d = mlb.csv2rec(fname, delimiter=' ')
-t_0 = float(sys.argv[2])
-#samples = d['v_drift'][np.where(3000.0 > d['t'] > t_0)]
-samples = d['v_drift'][np.where(np.logical_and(d['t'] > t_0, d['t'] < 5000.0))]
+t_0 = float(sys.argv[3])
+field = sys.argv[2]
+samples = d[field][np.where(d['t'] > t_0)]
 
 n = len(samples)
 mean = np.mean(samples)
 stderr = np.std(samples) / np.sqrt(n)
 fracerr = np.abs(stderr / mean)
 try:
-    if sys.argv[3] == '-v':
-        print('v_drift: %f\u00B1%f (%.1f%% error), t_start: %g' % (mean, stderr, 100.0 * fracerr, t_0))
+    if sys.argv[4] == '-v':
+        print('%s: %f\u00B1%f (%.1f%% error), t_start: %g' % (field, mean, stderr, 100.0 * fracerr, t_0))
     else:
         raise Exception
 except:
