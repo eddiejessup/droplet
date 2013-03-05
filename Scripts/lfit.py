7f80f0be7dfc2ee09bd767142b3d02b8e619f1d4
Elliot Marsden <elliot.marsden@gmail.com>
2013-03-05 12:51:30 +0000

    Make lfit more generic by not expecting x-value to be chi

--- Scripts/lfit.py
+++ Scripts/lfit.py
@@ -4,12 +4,13 @@ import numpy as np
 import matplotlib.mlab as mlb
 
 d = mlb.csv2rec(sys.argv[1], delimiter=' ')
-x = d['chi']
-y = d['v_drift']
-y_err = d['v_drift_err']
+x = d['x']
+field = sys.argv[2]
+y = d[field]
+y_err = d['%s_err' % field]
 
-p = np.poly1d(np.polyfit(x, y, 2, w=1.0/y_err**2))
+p = np.poly1d(np.polyfit(x, y, 1, w=1.0/y_err**2))
 
-print('chi v_drift v_drift_err v_drift_fit')
+print('x %s %s_err %s_fit' % (field, field, field))
 for x, y, y_err in zip(x, y, y_err):
     print(x, y, y_err, p(x))
