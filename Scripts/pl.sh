6d2b7c0cc2852f587ac77e1d07b9a161f807ff9f
Elliot Marsden <elliot.marsden@gmail.com>
2013-03-05 12:50:46 +0000

    Make plot script expect yerrorbars and plot them

--- Scripts/pl.sh
+++ Scripts/pl.sh
@@ -2,6 +2,10 @@
 
 c="plot "
 for f in $1*/log.csv; do
-    c+="'$f' u 't':'$2' w l ti '${f%/log.csv}', "
+    c+="'$f' u 't':'$2':'$2_err' w yerrorbars ti '${f%/log.csv}'"
+    if [ $# -gt 2 ]; then
+        c+=" w $3"
+    fi
+    c+=", "
 done
 echo ${c%, } | gnuplot -p
