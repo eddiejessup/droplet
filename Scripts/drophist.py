936885337eafb2fd78c7285e2fc3bcd7a8e4e7f2
Elliot Marsden <elliot.marsden@gmail.com>
2013-03-15 15:21:32 +0000

    Improve generally droplet distribution analysis script

--- Scripts/drophist.py
+++ Scripts/drophist.py
@@ -8,40 +8,28 @@ import matplotlib.pyplot as pp
 import numpy as np
 import utils
 
-n_bins = 30
+n_bins = 15
 
-print('l_rot vf acc acc_err t')
+if sys.argv[-1] == '-h':
+    print('l_rot vf acc acc_err t')
+    sys.argv = sys.argv[:-1]
+
 for dirname in sys.argv[1:]:
     fname = sorted(glob.glob('%s/r/*.npy' % dirname))[-1]
 
     yaml_args = yaml.safe_load(open('%s/params.yaml' % dirname, 'r'))
     n = int(yaml_args['particle_args']['n'])
-    try:
-        D_rot = yaml_args['particle_args']['motile_args']['rot_diff_args']['D_rot_0']
-    except KeyError:
-        D_rot = 0.0
-    if D_rot == 0.0: l_rot = np.inf
-    else: l_rot = 1.0 / D_rot
-    try:
-        r_c = float(yaml_args['particle_args']['collide_args']['R'])
-    except KeyError:
-        r_c = 0.0
-    vf = r_c ** 2 * n
+    R_drop = float(yaml_args['obstruction_args']['droplet_args']['R'])
+    l_rot = yaml_args['particle_args']['motile_args']['rot_diff_args']['l_rot_0']
+    vf = float(yaml_args['particle_args']['collide_args']['vf'])
 
     r = utils.vector_mag(np.load(fname))
-    f, R = np.histogram(r, bins=n_bins, range=[0.0, 1.0])
+    f, R = np.histogram(r, bins=n_bins, range=[0.0, R_drop])
     rho = f / (R[1:] ** 2 - R[:-1] ** 2)
 
-    bulk_rho = np.mean(rho[:-1])
-    if bulk_rho == 0.0:
-        bulk_rho_err = np.nan
-        acc = np.inf
-        acc_err = np.nan
-    else:
-        bulk_rho_err = np.std(rho[:-1])
-        acc = rho[-1] / bulk_rho
-        acc_err = acc * (bulk_rho_err / bulk_rho)
-    print('%f %f %f %f %s' % (l_rot, vf, acc, acc_err, os.path.splitext(os.path.basename(fname))[0]))
+    bulk_rho = n / (np.pi * R_drop ** 2)
+    acc = rho[-1] / bulk_rho
+    print('%f %f %f %s' % (l_rot, vf, acc, os.path.splitext(os.path.basename(fname))[0]))
 
 #    pp.bar(R[:-1], rho, width=(R[1]-R[0]))
 #    pp.xlim([0.0, 1.0])
