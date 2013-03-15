26d89cede7f2f30c8d5cb9ba6a822ea46def2be3
Elliot Marsden <elliot.marsden@gmail.com>
2013-03-15 15:32:47 +0000

    Modify droplet distribution script to handle more param combinations

--- Scripts/drophist.py
+++ Scripts/drophist.py
@@ -20,8 +20,30 @@ for dirname in sys.argv[1:]:
     yaml_args = yaml.safe_load(open('%s/params.yaml' % dirname, 'r'))
     n = int(yaml_args['particle_args']['n'])
     R_drop = float(yaml_args['obstruction_args']['droplet_args']['R'])
-    l_rot = yaml_args['particle_args']['motile_args']['rot_diff_args']['l_rot_0']
-    vf = float(yaml_args['particle_args']['collide_args']['vf'])
+    try:
+        rot_diff_args = yaml_args['particle_args']['motile_args']['rot_diff_args']
+    except KeyError:
+        l_rot = np.inf
+    else:
+        try:
+            l_rot = float(rot_diff_args['l_rot_0'])
+        except KeyError:
+            D_rot = float(yaml_args['particle_args']['motile_args']['rot_diff_args']['D_rot_0'])
+            v_0 = float(yaml_args['particle_args']['motile_args']['v_0'])
+            try:
+                l_rot = v_0 / D_rot_0
+            except ZeroDivisionError:
+                l_rot = np.inf
+    try:
+        collide_args = yaml_args['particle_args']['collide_args']
+    except KeyError:
+        vf = 0.0
+    else:
+        try:
+            vf = float(collide_args['vf'])
+        except KeyError:
+            r_c = float(yaml_args['particle_args']['collide_args']['R'])
+            vf = n * (r_c / R_drop) ** 2
 
     r = utils.vector_mag(np.load(fname))
     f, R = np.histogram(r, bins=n_bins, range=[0.0, R_drop])
