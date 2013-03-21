4fdb500872a5109c49af11bc473daf59c2b50fa4
Elliot Marsden <elliot.marsden@gmail.com>
2013-03-21 23:47:41 +0000

    Add temporal plotting option to logread script

--- Scripts/lfit.py
+++ Scripts/lfit.py
@@ -4,7 +4,6 @@ import sys
 import numpy as np
 import matplotlib.mlab as mlb
 
-
 parser = argparse.ArgumentParser(description='Fit to some datapoints')
 parser.add_argument('f',
     help='Datapoints file')
@@ -12,8 +11,6 @@ parser.add_argument('field',
     help='Name of field to use as y')
 parser.add_argument('-t', '--header', default=False, action='store_true',
     help='whether to output header, default is false')
-parser.add_argument('-p', '--plot', default=False, action='store_true',
-    help='whether to plot distribution, default is false')
 parser.add_argument('-o', '--order', type=float, default=1,
     help='order of polynomial to fit, default is 1, i.e. linear fit')
 args = parser.parse_args()
@@ -25,6 +22,7 @@ y_err = d['%s_err' % args.field]
 
 p = np.poly1d(np.polyfit(x, y, args.order, w=1.0/y_err**2))
 
-print('x %s %s_err %s_fit' % (args.field, args.field, args.field))
+print(p)
+if args.header: print('x %s %s_err %s_fit' % (args.field, args.field, args.field))
 for x, y, y_err in zip(x, y, y_err):
     print(x, y, y_err, p(x))
--- Scripts/logread.py
+++ Scripts/logread.py
@@ -1,7 +1,10 @@
 #! /usr/bin/python3
 import os
 import argparse
+import numpy as np
 import matplotlib.mlab as mlb
+import matplotlib as mpl
+import matplotlib.pyplot as pp
 
 parser = argparse.ArgumentParser(description='Analyse log files')
 parser.add_argument('field', 
@@ -10,18 +13,40 @@ parser.add_argument('dirs', nargs='*',
     help='Directories')
 parser.add_argument('-t', '--header', default=False, action='store_true',
     help='whether to output header, default is false')
-
+parser.add_argument('-p', '--plot', default=False, action='store_true',
+    help='whether to plot logs over time, default is false')
+parser.add_argument('-r', '--repeat', default=False, action='store_true',
+    help='whether to prompt to refresh the data')
 args = parser.parse_args()
 
-if args.header:
+def main(args):
+    for dirname in args.dirs:
+        if not os.path.isdir(dirname): continue
+
+        fname = '%s/log.csv' % dirname
+        d = mlb.csv2rec(fname, delimiter=' ')
+
+        xs, ys, ys_err = d['t'], d[args.field.lower()], d['%s_err' % args.field.lower()]
+        i = np.where(xs > 50.0)[0][0]
+        xs, ys, ys_err = xs[i:], ys[i:], ys_err[i:]
+
+        if not args.plot:
+            print('%s %f %f' % (dirname, ys[-1], ys_err[-1]))
+        else:
+            pp.errorbar(xs, ys, yerr=ys_err, label='%s' % dirname)
+
+    if args.plot:
+        pp.xlabel('t')
+        pp.ylabel(args.field)
+        pp.legend()
+        pp.show()
+
+if not args.plot and args.header:
     print('x %s %s_err' % (args.field, args.field))
 
-for dirname in args.dirs:
-    if not os.path.isdir(dirname): continue
-
-    fname = '%s/log.csv' % dirname
-    d = mlb.csv2rec(fname, delimiter=' ')
-
-    y = d[args.field][-1]
-    y_err = d['%s_err' % args.field][-1]
-    print('%s %f %f' % (dirname, y, y_err))
+if args.repeat:
+    while True:
+        main(args)
+        input('Enter to refresh...')
+else:
+    main(args)
