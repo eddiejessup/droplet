2ad0ff40908008389e243bc1a3d2494ced23a0de
Elliot Marsden <elliot.marsden@gmail.com>
2013-07-25 16:58:53 +0100

    Remove profiling from mrun as you can do just as good a job using python -m cProfile <script.py>

--- Source/mrun.py
+++ Source/mrun.py
@@ -2,7 +2,6 @@
 
 from __future__ import print_function
 import argparse
-import cProfile
 import pstats
 import datetime
 import yaml
@@ -26,61 +25,46 @@ parser.add_argument('-l', '--latest', default=False, action='store_true',
     help='only keep output of latest system configuration, default is false')
 parser.add_argument('-s', '--silent', default=False, action='store_true',
     help='don''t print to stdout')
-parser.add_argument('-p', '--profile', default=False, action='store_true',
-    help='profile program (implies -s -d None)')
 
 args = parser.parse_args()
 
-if args.profile and args.runtime == float('inf'):
-    raise Exception('Cannot profile a simulation run without a specified run-time')
+if not args.silent: print('\n' + 5*'*' + ' Bannock simulation ' + 5*'*' + '\n')
+yaml_args = yaml.safe_load(open(args.f, 'r'))
 
-def main():
-    yaml_args = yaml.safe_load(open(args.f, 'r'))
-
-    if args.dir is not None:
-        if not args.silent: print('Initialising output...')
-        utils.makedirs_safe(args.dir)
-        git_hash = utils.get_git_hash()
-        yaml_args['about_args'] = {'git_hash': git_hash, 'started': str(datetime.datetime.now())}
-        yaml.dump(yaml_args, open('%s/params.yaml' % args.dir, 'w'))
-        if not args.silent: print('done!\n')
-
-    if not args.silent: print('Initialising system...')
-    env = environment.Environment(**yaml_args)
+if args.dir is not None:
+    if not args.silent: print('Initialising output...')
+    utils.makedirs_safe(args.dir)
+    git_hash = utils.get_git_hash()
+    yaml_args['about_args'] = {'git_hash': git_hash, 'started': str(datetime.datetime.now())}
+    yaml.dump(yaml_args, open('%s/params.yaml' % args.dir, 'w'))
     if not args.silent: print('done!\n')
 
-    if args.dir is not None:
-        if not args.silent: print('Outputting static data...')
-        env.checkpoint('%s/initial.pkl' % args.dir)
-        utils.makedirs_soft('%s/dyn' % args.dir)
-        if args.cpevery != -1:
-            utils.makedirs_soft('%s/cp' % args.dir)
-        if not args.silent: print('done!\n')
+if not args.silent: print('Initialising system...')
+env = environment.Environment(**yaml_args)
+if not args.silent: print('done!\n')
 
-    if not args.silent: print('Iterating system...')
-    while env.t < args.runtime:
-        if not env.i % args.every:
-            if not args.silent:
-                print('\tt:%010g i:%08i...' % (env.t, env.i), end='')
-            if args.dir is not None:
-                out_fname = 'latest' if args.latest else '%010f' % env.t
-                env.output('%s/dyn/%s' % (args.dir, out_fname))
-            if not args.silent: print('done!')
-        if args.cpevery != -1 and not env.i % args.cpevery:
-            print('MAKING CHECKPOINT...', end='')
-            out_fname = '%010f' % env.t
-            env.checkpoint('%s/cp/%s' % (args.dir, out_fname))
-            print('done!')
-
-        env.iterate()
+if args.dir is not None:
+    if not args.silent: print('Outputting static data...')
+    env.checkpoint('%s/initial' % args.dir)
+    utils.makedirs_soft('%s/dyn' % args.dir)
+    if args.cpevery != -1:
+        utils.makedirs_soft('%s/cp' % args.dir)
     if not args.silent: print('done!\n')
 
-if args.profile:
-    args.silent = True
-    args.dir = None
-    cProfile.run('main()', 'prof')
-    p = pstats.Stats('prof')
-    p.strip_dirs().sort_stats('cum').print_stats()
-else:
-    if not args.silent: print('\n' + 5*'*' + ' Bannock simulation ' + 5*'*' + '\n')
-    main()
+if not args.silent: print('Iterating system...')
+while env.t < args.runtime:
+    if not env.i % args.every:
+        if not args.silent:
+            print('\tt:%010g i:%08i...' % (env.t, env.i), end='')
+        if args.dir is not None:
+            out_fname = 'latest' if args.latest else '%010f' % env.t
+            env.output('%s/dyn/%s' % (args.dir, out_fname))
+        if not args.silent: print('done!')
+    if args.cpevery != -1 and not env.i % args.cpevery:
+        print('MAKING CHECKPOINT...', end='')
+        out_fname = '%010f' % env.t
+        env.checkpoint('%s/cp/%s' % (args.dir, out_fname))
+        print('done!')
+
+    env.iterate()
+if not args.silent: print('done!\n')
