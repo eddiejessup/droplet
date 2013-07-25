e385fccdb69928cc4d28cd0db9114e22006cef0c
Elliot Marsden <elliot.marsden@gmail.com>
2013-07-25 18:43:33 +0100

    Add awesome checkpoint resuming to mrun

--- Source/mrun.py
+++ Source/mrun.py
@@ -9,18 +9,20 @@ import numpy as np
 import utils
 import obstructions
 import environment
+import os
+import pickle
 
 parser = argparse.ArgumentParser(description='Run a particle simulation')
 parser.add_argument('f',
-    help='YAML file containing system parameters')
+    help='Either a YAML file containing system parameters or an existing output data directory')
 parser.add_argument('-t', '--runtime', type=float, default=float('inf'),
     help='how long to run, default is forever')
 parser.add_argument('-d', '--dir', default=None,
     help='output directory, default is no output')
 parser.add_argument('-e', '--every', type=int, default=1,
     help='how many iterations should elapse between outputs, default is 1')
-parser.add_argument('-c', '--cpevery', type=int, default=-1,
-    help='how many iterations should elapse between checkpoints, default is never')
+parser.add_argument('-c', '--cp', type=int, default=-1,
+    help='how many iterations should elapse between checkpoints, negative means never')
 parser.add_argument('-l', '--latest', default=False, action='store_true',
     help='only keep output of latest system configuration, default is false')
 parser.add_argument('-s', '--silent', default=False, action='store_true',
@@ -28,28 +30,45 @@ parser.add_argument('-s', '--silent', default=False, action='store_true',
 
 args = parser.parse_args()
 
+if args.cp == -1:
+    raw_input('Warning: no checkpointing requested, press enter to continue anyway...')
+
 if not args.silent: print('\n' + 5*'*' + ' Bannock simulation ' + 5*'*' + '\n')
-yaml_args = yaml.safe_load(open(args.f, 'r'))
+
+resume = os.path.isdir(args.f)
+
+if not resume:
+    yaml_args = yaml.safe_load(open(args.f, 'r'))
+else:
+    # if resuming, output dir is the input dir
+    args.dir = args.f
+    yaml_args = yaml.safe_load(open('%s/params.yaml' % args.dir, 'r'))
 
 if args.dir is not None:
     if not args.silent: print('Initialising output...')
-    utils.makedirs_safe(args.dir)
-    git_hash = utils.get_git_hash()
-    yaml_args['about_args'] = {'git_hash': git_hash, 'started': str(datetime.datetime.now())}
-    yaml.dump(yaml_args, open('%s/params.yaml' % args.dir, 'w'))
+
+    if not resume:
+        utils.makedirs_safe(args.dir)
+        utils.makedirs_soft('%s/dyn' % args.dir)
+        yaml_args['about_args'] = {}
+
+    start_num = 0
+    while True:
+        header = 'start_%i' % start_num
+        if header not in yaml_args['about_args']:
+            break
+        start_num += 1
+
+    yaml_args['about_args'][header] = about_args = {}
+    about_args['git_hash'] = utils.get_git_hash()
+    about_args['started'] = str(datetime.datetime.now())
+    yaml.dump(yaml_args, open('%s/params.yaml' % args.dir, 'w'), default_flow_style=False)
     if not args.silent: print('done!\n')
 
 if not args.silent: print('Initialising system...')
-env = environment.Environment(**yaml_args)
+if not resume: env = environment.Environment(**yaml_args)
+else: env = pickle.load(open('%s/cp.pkl' % args.dir, 'rb'))
 if not args.silent: print('done!\n')
-
-if args.dir is not None:
-    if not args.silent: print('Outputting static data...')
-    env.checkpoint('%s/initial' % args.dir)
-    utils.makedirs_soft('%s/dyn' % args.dir)
-    if args.cpevery != -1:
-        utils.makedirs_soft('%s/cp' % args.dir)
-    if not args.silent: print('done!\n')
 
 if not args.silent: print('Iterating system...')
 while env.t < args.runtime:
@@ -60,10 +79,9 @@ while env.t < args.runtime:
             out_fname = 'latest' if args.latest else '%010f' % env.t
             env.output('%s/dyn/%s' % (args.dir, out_fname))
         if not args.silent: print('done!')
-    if args.cpevery != -1 and not env.i % args.cpevery:
-        print('MAKING CHECKPOINT...', end='')
-        out_fname = '%010f' % env.t
-        env.checkpoint('%s/cp/%s' % (args.dir, out_fname))
+    if env.i == 0 or (args.cp > 0 and not env.i % args.cp):
+        print('Making checkpoint...', end='')
+        env.checkpoint('%s/cp' % args.dir)
         print('done!')
 
     env.iterate()
