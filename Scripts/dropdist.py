d885b76b728386eb0cd8cd391297d5341c5155da
Elliot Marsden <elliot.marsden@gmail.com>
2014-06-17 13:55:29 +0100

    Remove old dropdist and move new to old filename

--- Scripts/dropdist.py
+++ Scripts/dropdist.py
@@ -1,116 +1,75 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 
 
 import argparse
 import numpy as np
-import matplotlib.pyplot as pp
 import utils
-# import ejm_rcparams
 import droplyse
 import scipy.stats
 import glob
-from dropplot import cs
-
-fs = 20
+import os
 
 
-def plot(dirs, bins, res, theta_max):
-    for i_ds, fnames in enumerate(dirs):
-        # if len(fnames) == 1:
-        #     fnames = glob.glob('{}/dyn/*.npz'.format(fnames[0]))
-        # pp.subplot(3, 3, i_ds+1)
-        Rs_edges, R_drops, n_s, ns_s = [], [], [], []
-        for i_d, fname in enumerate(fnames):
-            # if i_ds == 0:
-            #     theta_max = np.pi / 2.0
-            # elif i_ds == 1:
-            #     theta_max = np.pi / 2.0
-            # elif i_ds == 2:
-            #     theta_max = np.pi / 3.0
-            # elif i_ds == 3:
-            #     theta_max = np.pi / 4.0
-            # elif i_ds == 4:
-            #     theta_max = np.pi / 5.0
-            # elif i_ds == 5:
-            #     theta_max = np.pi / 6.0
-
-            import os
+def rdf(dirname, bins, res, theta_max):
+    fnames = glob.glob('{}/dyn/*.npz'.format(dirname))
+    Rs_edges, R_drops, n_s, ns_s = [], [], [], []
+    for i_d, fname in enumerate(fnames):
+        if not droplyse.is_hemisphere(fname):
             pre, ext = os.path.splitext(os.path.basename(fname))
-            if ext == '.npz':
-                t = float(pre)
-                if t < 20.0:
-                    # print('skip ', t )
-                    continue
+            t = float(pre)
+            if t < 20.0:
+                continue
 
-            xyz, R_drop, hemisphere = droplyse.parse(fname, theta_max)
-            # if i_ds == 0:
-            #     hemisphere = False
-            n = len(xyz)
-            r = utils.vector_mag(xyz)
+        xyz, R_drop, hemisphere = droplyse.parse(fname, theta_max)
+        n = len(xyz)
+        r = utils.vector_mag(xyz)
 
-            Rs_edge, ns = droplyse.make_hist(r, R_drop, bins, res)
+        if res is not None:
+            bins = droplyse.res_to_bin(droplyse.buff * R_drop, res)
+        ns, R_edges = np.histogram(r, bins=bins, range=[0.0, droplyse.buff * R_drop])
 
-            Rs_edges.append(Rs_edge)
-            R_drops.append(R_drop)
-            ns_s.append(ns)
-            n_s.append(n)
+        Rs_edges.append(R_edges)
+        R_drops.append(R_drop)
+        ns_s.append(ns)
+        n_s.append(n)
 
-        Rs_edge = np.mean(Rs_edges, axis=0)
-        R_drop = np.mean(R_drops)
-        ns = np.mean(ns_s, axis=0)
-        ns_err = scipy.stats.sem(ns_s, axis=0)
-        n = np.mean(n_s)
+    R_edges = np.mean(Rs_edges, axis=0)
+    R_drop = np.mean(R_drops)
+    ns = np.mean(ns_s, axis=0)
+    ns_err = scipy.stats.sem(ns_s, axis=0)
+    n = np.mean(n_s)
 
-        Vs_edge, rhos = droplyse.n_to_rho(Rs_edge, ns, droplyse.dim, hemisphere, theta_max)
-        Vs_edge, rhos_err = droplyse.n_to_rho(Rs_edge, ns_err, droplyse.dim, hemisphere, theta_max)
-        rho_0 = droplyse.n0_to_rho0(n, R_drop, droplyse.dim, hemisphere, theta_max)
-        vf = rho_0 * droplyse.V_particle
-        R_peak, n_peak = droplyse.peak_analyse(Rs_edge, ns, n, R_drop, 'mean', droplyse.dim, hemisphere, theta_max)
+    Vs_edge, rhos = droplyse.n_to_rho(R_edges, ns, droplyse.dim, hemisphere, theta_max)
+    Vs_edge, rhos_err = droplyse.n_to_rho(R_edges, ns_err, droplyse.dim, hemisphere, theta_max)
+    rho_0 = droplyse.n0_to_rho0(n, R_drop, droplyse.dim, hemisphere, theta_max)
 
-        Rs = 0.5 * (Rs_edge[:-1] + Rs_edge[1:])
+    Rs = 0.5 * (R_edges[:-1] + R_edges[1:])
 
-        c = cs[i_ds]
-        # c = 'red'
-        # c = 'blue'
-        # if i_ds == 0:
-        #     label = r'$\theta_\mathrm{max} = \pi$'
-        # else:
-        #     label = r'$\theta_\mathrm{max} = \pi / %d$' % (i_ds + 1)
-        # label = r'$R=\SI{%.3g}{\um}, \phi=\SI{%.2g}{\percent}$' % (R_drop, 100.0 * vf)
-        label = r'R=%.3g, phi=%.2g$' % (R_drop, 100.0 * vf)
-        # if i_ds == 0:
-        #     label += r' Smooth Swimming'
-        # else:
-        #     label += r' Wild Type'
-        pp.errorbar(Rs / R_drop, rhos / rho_0,
-                    yerr=rhos_err / rho_0, label=label, c=c)
-        pp.axvline(R_peak / R_drop, c=c)
-        pp.axhline(1.0, c=c)
-
-        pp.ylim(0.0, 4.5)
-        pp.xlim(0.0, droplyse.buff)
-        # pp.yticks(np.arange(0.0, 5.001, 1.0))
-        # pp.tick_params(labelsize=10)
-        pp.xlabel(r'$r / \mathrm{R}$', fontsize=fs)
-        pp.ylabel(r'$\rho(r) / \rho_0$', fontsize=fs)
-        # pp.legend(loc='upper left', fontsize=fs-12)
-        pp.legend(loc='upper left', fontsize=fs)
-    pp.show()
-    # pp.savefig('grid.pdf', bbox_inches='tight')
+    Rs_norm = Rs / R_drop
+    rhos_norm = rhos / rho_0
+    rhos_norm_err = rhos_err / rho_0
+    return Rs_norm, rhos_norm, rhos_norm_err
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Analyse droplet distributions')
-    parser.add_argument('-d', '--dirs', nargs='*', action='append',
-                        help='Data directories')
+    parser.add_argument('dir',
+                        help='Data directory')
     parser.add_argument('-b', '--bins', type=int,
                         help='Number of bins to use')
     parser.add_argument('-r', '--res', type=float,
                         help='Bin resolution in micrometres')
-    parser.add_argument('--theta_factor', type=float, default=2.0,
+    parser.add_argument('-t', '--theta_factor', type=float, default=2.0,
                         help='Solid angle in reciprocal factor of pi')
+    parser.add_argument('-o', '--out',
+                        help='Output file')
+
     args = parser.parse_args()
 
+    if args.out is None:
+        args.out = args.dir + '_rdf.txt'
+
     theta_max = np.pi / args.theta_factor
-    plot(args.dirs, args.bins, args.res, theta_max)
+    Rs_norm, rhos_norm, rhos_norm_err = rdf(args.dir, args.bins, args.res, theta_max)
+    np.savetxt(args.out, list(zip(Rs_norm, rhos_norm, rhos_norm_err)), header='R_norm rho_norm rho_norm_err')
