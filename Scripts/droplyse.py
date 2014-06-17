7db8d4681730f2cb56b7ec16c8f2af53bcf78c2d
Elliot Marsden <elliot.marsden@gmail.com>
2014-06-17 17:35:49 +0100

    Add errors to droplyse

--- Scripts/droplyse.py
+++ Scripts/droplyse.py
@@ -21,6 +21,12 @@ A_bug = np.pi * R_bug ** 2
 dim = 3
 
 
+def qsum(*args):
+    if len(args) == 1:
+        return np.sqrt(np.sum(np.square(args[0])))
+    return np.sqrt(np.sum(np.square(args)))
+
+
 def get_f_peak_uni(R_peak, R_drop, theta_max, hemisphere):
     V_drop = V_sector(R_drop, theta_max, hemisphere)
     V_bulk = V_sector(R_peak, theta_max, hemisphere)
@@ -127,6 +133,7 @@ def peak_analyse(Rs_edge, ns, ns_err, n, R_drop, alg, dim, hemisphere, theta_max
         Rs_int = line_intersections_up(Rs, rhos, rho_base)
         try:
             R_peak = Rs_int[-1]
+            R_peak_err = (Rs_edge[1] - Rs_edge[0]) / 2.0
         except IndexError:
             raise Exception(hemisphere)
     elif alg == 'median':
@@ -142,7 +149,7 @@ def peak_analyse(Rs_edge, ns, ns_err, n, R_drop, alg, dim, hemisphere, theta_max
     try:
         i_peak = np.where(Rs >= R_peak)[0][0]
     except IndexError:
-        i_peak = R_peak = n_peak = np.nan
+        i_peak = R_peak = R_peak_err = n_peak = n_peak_err = np.nan
     else:
         n_peak = ns[i_peak:].sum()
         n_peak_err = np.sqrt(np.sum(np.square(ns_err[i_peak:])))
@@ -186,8 +193,9 @@ def analyse_many(dirnames, bins, res, alg, theta_max):
     ns_err = st.sem(np.array(ns_s), axis=0)
     R_peak, n_peak, n_peak_err = peak_analyse(
         R_edges, ns, ns_err, n, R_drop, args.alg, dim, hemisphere, theta_max)
+    R_peak_err = (R_edges[1] - R_edges[0]) / 2.0
 
-    return R_drop, hemisphere, n, n_err, r_mean, r_mean_err, r_var, r_var_err, R_peak, n_peak, n_peak_err
+    return R_drop, hemisphere, n, n_err, r_mean, r_mean_err, r_var, r_var_err, R_peak, R_peak_err, n_peak, n_peak_err
 
 
 if __name__ == '__main__':
@@ -209,36 +217,73 @@ if __name__ == '__main__':
     if args.bins is None and args.res is None:
         raise Exception('Require either bin number or resolution')
 
-    print('R_drop hemisphere vp r_mean r_var R_peak V_drop V_peak V_bulk n n_peak n_bulk rho_0 rho_peak rho_bulk f_peak f_bulk eta_0 eta f_peak_uni f_peak_excess')
+    print('R_drop hemisphere vp vp_err r_mean r_mean_err r_var r_var_err R_peak R_peak_err V_drop V_drop_err V_peak V_peak_err V_bulk V_bulk_err n n_err n_peak n_peak_err n_bulk n_bulk_err rho_0 rho_0_err rho_peak rho_peak_err rho_bulk rho_bulk_err f_peak f_peak_err f_bulk f_bulk_err eta_0 eta_0_err eta eta_err f_peak_uni f_peak_uni_err f_peak_excess f_peak_excess_err')
     for bdn in args.bdns:
         ignores = ['118', '119', '121', '124', '223', '231', '310', '311']
         if any([ig in bdn for ig in ignores]):
             continue
 
         dirnames = glob.glob(os.path.join(bdn, 'dyn/*.npz'))
-        R_drop, hemisphere, n, n_err, r_mean, r_mean_err, r_var, r_var_err, R_peak, n_peak, n_peak_err = analyse_many(dirnames, args.bins, args.res, args.alg, theta_max)
+        R_drop, hemisphere, n, n_err, r_mean, r_mean_err, r_var, r_var_err, R_peak, R_peak_err, n_peak, n_peak_err = analyse_many(dirnames, args.bins, args.res, args.alg, theta_max)
+        R_drop_err = 0.0
 
         rho_0 = n0_to_rho0(n, R_drop, dim, hemisphere, theta_max)
+        rho_0_err = n0_to_rho0(n_err, R_drop, dim, hemisphere, theta_max)
 
         V_drop = V_sector(R_drop, theta_max, hemisphere)
+        V_drop_err = V_drop * 3.0 * (R_drop_err / R_drop)
 
         V_bulk = V_sector(R_peak, theta_max, hemisphere)
+        V_bulk_err = V_bulk * 3.0 * (R_peak_err / R_peak)
         n_bulk = n - n_peak
-        rho_bulk = n_bulk / V_bulk
+        n_bulk_err = n_err
+        rho_bulk = (n_bulk / V_bulk) / rho_0
+        rho_bulk_err = rho_bulk * qsum(n_bulk_err / n_bulk, rho_0_err / rho_0)
 
         V_peak = V_drop - V_bulk
-        rho_peak = n_peak / V_peak
+        V_peak_err = V_peak * 3.0 * (R_peak_err / R_peak)
+        rho_peak = (n_peak / V_peak) / rho_0
+        rho_peak_err = rho_peak * qsum(n_peak_err / n_peak, rho_0_err / rho_0)
 
         f_peak = n_peak / n
+        f_peak_err = f_peak * qsum(n_peak_err / n_peak, n_err / n)
         f_bulk = n_bulk / n
+        f_bulk_err = f_bulk * qsum(n_bulk_err / n_bulk, n_err / n)
 
         vf = rho_0 * V_particle
+        vf_err = rho_0_err * V_particle
         vp = 100.0 * vf
+        vp_err = 100.0 * vf_err
 
         eta = n_to_eta(n_peak, R_drop, theta_max, hemisphere)
+        eta_err = n_to_eta(n_peak_err, R_drop, theta_max, hemisphere)
         eta_0 = n_to_eta(n, R_drop, theta_max, hemisphere)
+        eta_0_err = n_to_eta(n_err, R_drop, theta_max, hemisphere)
 
         f_peak_uni = get_f_peak_uni(R_peak, R_drop, theta_max, hemisphere)
-        f_peak_excess = f_peak - f_peak_uni
+        f_peak_uni_err = 0.0
 
-        print(R_drop, hemisphere, vp, r_mean, r_var, R_peak, V_drop, V_peak, V_bulk, n, n_peak, n_bulk, rho_0, rho_peak, rho_bulk, f_peak, f_bulk, eta_0, eta, f_peak_uni, f_peak_excess)
+        f_peak_excess = f_peak - f_peak_uni
+        f_peak_excess_err = f_peak_err
+
+        print(R_drop,
+              str(int(hemisphere)),
+              vp, vp_err,
+              r_mean, r_mean_err,
+              r_var, r_var_err,
+              R_peak, R_peak_err,
+              V_drop, V_drop_err,
+              V_peak, V_peak_err,
+              V_bulk, V_bulk_err,
+              n, n_err,
+              n_peak, n_peak_err,
+              n_bulk, n_bulk_err,
+              rho_0, rho_0_err,
+              rho_peak, rho_peak_err,
+              rho_bulk, rho_bulk_err,
+              f_peak, f_peak_err,
+              f_bulk, f_bulk_err,
+              eta_0, eta_0_err,
+              eta, eta_err,
+              f_peak_uni, f_peak_uni_err,
+              f_peak_excess, f_peak_excess_err)
