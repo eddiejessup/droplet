befc2722b3ea5231ea1e1424725e038d55c356d6
Elliot Marsden <elliot.marsden@gmail.com>
2014-07-10 13:14:48 +0100

    Simplify breg since we're not incrementally fitting any more

--- Scripts/breg.py
+++ Scripts/breg.py
@@ -1,107 +1,52 @@
 #!/usr/bin/env python3
 
-
-import matplotlib.pyplot as pp
+import matplotlib.pyplot as plt
 import numpy as np
-import droplyse
 import scipy.optimize
 import argparse
-import functools
 
 
-def f_full(eta_0, b, c):
+f_macro_exp = '/Users/ejm/Projects/Bannock Data/Drop/Experiment/smooth/Analysis/experiment.txt'
+
+
+def f_peak_eq(eta_0, b, c):
     coeffs = np.array([eta_0, -(1.0 + eta_0), (1.0 - c - b * eta_0)]).T
     return np.array([np.roots(co)[-1].real for co in coeffs])
 
 
-def f_approx(eta_0, b, c):
-    return 1.0 - c - b * eta_0
+def fit(datname):
+    d = np.genfromtxt(datname, names=True)
 
+    eta_0, f_peak, f_peak_err = d['eta_0'], d['f_peak'], d['f_peak_err']
 
-def make_f(c_fix=None, approx=False):
-    if approx:
-        f = f_approx
-    else:
-        f = f_full
-    if c_fix is not None:
-        return functools.partial(f, c=c_fix)
-    else:
-        return f
+    popt, pcov = scipy.optimize.curve_fit(f_peak_eq, eta_0, f_peak, sigma=1.0 / f_peak_err)
+    b, b_err = popt[0], np.sqrt(pcov[0, 0])
+    c, c_err = popt[1], np.sqrt(pcov[1, 1])
 
+    return eta_0, f_peak, f_peak_err, b, b_err, c, c_err
 
-def fit(datname, approx=False, c_fix=0.15):
-    try:
-        dat = np.loadtxt(datname, delimiter=' ')
-    except ValueError:
-        dat = np.loadtxt(datname, delimiter=',')
-        eta_0, eta_0_err, eta_f, eta_f_err = dat.T
-    else:
-        peakies = np.logical_not(np.any(np.isnan(dat), axis=1))
-        dat = dat[peakies].T
-
-        (n, n_err, R_drop, r_mean, r_mean_err, r_var, r_var_err,
-         R_peak, R_peak_err, n_peak, n_peak_err, hemisphere,
-         theta_max) = dat
-
-        hemisphere = hemisphere[0]
-        theta_max = theta_max[0]
-
-        eta = droplyse.n_to_eta(n_peak, R_drop, theta_max, hemisphere)
-        eta_0 = droplyse.n_to_eta(n, R_drop, theta_max, hemisphere)
-
-        eta_f = eta / eta_0
-
-    f = make_f(c_fix, approx)
-
-    i_sort = np.argsort(eta_0)
-    eta_0 = eta_0[i_sort]
-    eta_f = eta_f[i_sort]
-
-    p0 = [0.5] if c_fix is not None else [0.5, 0.5]
-
-    dats = []
-    for i in range(3, len(eta_0) + 1):
-        # print(eta_0[:i])
-
-        popt, pcov = scipy.optimize.curve_fit(f, eta_0[:i], eta_f[:i], p0=p0)
-        b, b_err = popt[0], np.sqrt(pcov[0, 0])
-        if c_fix is not None:
-            c, c_err = c_fix, 0.0
-        else:
-            c, c_err = popt[1], np.sqrt(pcov[1, 1])
-
-        dat = eta_0[i - 1], b, b_err, c, c_err
-        dats.append(dat)
-
-        x = np.linspace(min(eta_0), max(eta_0), 500)
-        if c_fix is None:
-            y = f(x, b, c)
-            eta_f_fit = f(eta_0[:i], b, c)
-        else:
-            y = f(x, b)
-            eta_f_fit = f(eta_0[:i], b, c)
-
-        sst = np.sum(np.square(eta_f - np.mean(eta_f)))
-        ssreg = np.sum(np.square(eta_f_fit - np.mean(eta_f)))
-        Rsq = ssreg / sst
-
-        pp.plot(x, y)
-        # pp.xscale('log')
-        pp.scatter(eta_0[:i], eta_f[:i], c='red')
-        pp.scatter(eta_0[i:], eta_f[i:], c='blue')
-        pp.ylim(0.0, 1.0)
-        # pp.show()
-
-    return np.array(dats)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Plot droplet analysis files')
-    parser.add_argument('datname', help='Data file')
-    parser.add_argument('--approx', default=False, action='store_true')
-    parser.add_argument('-c', type=float)
+    parser.add_argument('datname', nargs='*', help='Data file')
     args = parser.parse_args()
 
-    dats = fit(args.datname, args.approx, args.c)
-    for dat in dats:
-        print(*dat)
+    eta_0, f_peak, f_peak_err, b, b_err, c, c_err = fit(args.datname[0])
+    eta_0_fit = np.linspace(2e-3, 0.5, 500)
+    plt.plot(eta_0_fit, f_peak_eq(eta_0_fit, b, c), color='blue')
+    plt.errorbar(eta_0, f_peak, yerr=f_peak_err, ls='none', color='blue')
+
+    eta_0, f_peak, f_peak_err, b, b_err, c, c_err = fit(args.datname[1])
+    eta_0_fit = np.linspace(2e-3, 0.5, 500)
+    plt.plot(eta_0_fit, f_peak_eq(eta_0_fit, b, c), color='green')
+    plt.errorbar(eta_0, f_peak, yerr=f_peak_err, ls='none', color='green')
+
+    eta_0, f_peak, f_peak_err, b, b_err, c, c_err = fit(f_macro_exp)
+    eta_0_fit = np.linspace(2e-3, 0.5, 500)
+    plt.plot(eta_0_fit, f_peak_eq(eta_0_fit, b, c), color='red')
+    plt.errorbar(eta_0, f_peak, yerr=f_peak_err, ls='none', color='red')
+
+    plt.xscale('log')
+    plt.ylim(0.0, 1.0)
+    plt.show()
