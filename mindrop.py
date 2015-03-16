74969f7b971b560910ac5e11759dd2775fdbce4e
Elliot Marsden <elliot.marsden@gmail.com>
2015-03-16 13:27:55 +0000

    Add droplet and neighbour forces

--- mindrop.py
+++ mindrop.py
@@ -12,38 +12,36 @@ def collisions(r, u, l, R, R_d):
     return geom.capsule_intersection(r, u, l, R, L=2.2 * R_d)
 
 
-def do_forces(r, u, l, R, R_d, r_old, u_old):
-    # Droplet check
-    c_drop = numerics.capsule_obstructed(r, u, l, R, R_d)
-
-    # Alignment
-    u_r = vector.vector_unit_nonull(r[c_drop])
-    u_dot_u_r = np.sum(u[c_drop] * u_r, axis=-1)
-    u[c_drop] = vector.vector_unit_nonull(u[c_drop] - u_r *
-                                          u_dot_u_r[:, np.newaxis])
-
-    # Translation
-    r[c_drop] = u_r * spherocylinder_distance(R_d, l, R)
-
-    reverts = np.zeros([len(r)], dtype=np.bool)
-    while True:
-        # Neighbour check
-        c_neighb = collisions(r, u, l, R, R_d)
-
-        if not np.any(c_neighb):
-            break
-        reverts += c_neighb
-        r[c_neighb], u[c_neighb] = r_old[c_neighb], u_old[c_neighb]
-    return reverts
+def force(r_sq, w, E, sigma):
+    F = np.zeros(r_sq.shape)
+    c = r_sq > w ** 2
+    F[c] = E * (np.exp((np.sqrt(r_sq[c]) - w) / sigma) - 1.0)
+    return F
 
 
-def dropsim(n, v, l, R, D, Dr, R_d, dim, t_max, dt, out, every, Dr_c):
+def get_drop_force(r, u, l, R, R_d, E_drop, sigma_drop):
+    r_rad_sq = numerics.capsule_radial_distance_sq(r, u, l, R, R_d)
+    F = force(r_rad_sq, R_d - R, E_drop, sigma_drop)
+    return -F[:, np.newaxis] * vector.vector_unit_nonull(r)
+
+
+def get_neighb_force(r, u, l, R, R_d, E_neighb, sigma_neighb):
+    F, c_neighb = numerics.capsule_neighb_force(r, u, l, R, 2.2 * R_d,
+                                                E_neighb, sigma_neighb)
+    return -F, c_neighb
+
+
+def dropsim(n, v, l, R, D, Dr, R_d, dim, t_max, dt, out, every, Dr_c,
+            E, sigma, alpha):
     if out is not None:
         fileio.makedirs_safe(out)
         fileio.makedirs_soft('%s/dyn' % out)
 
     r = np.random.uniform(-R_d, R_d, size=[n, dim])
     u = vector.sphere_pick(dim, n)
+
+    # Hydrodynamic prefactor
+    v_hydro_0 = (3.0 / 16.0) * alpha * R ** 2 * v
 
     for i in range(n):
         while True:
@@ -62,29 +60,23 @@ def dropsim(n, v, l, R, D, Dr, R_d, dim, t_max, dt, out, every, Dr_c):
     i = 0
     t = 0
     while t < t_max:
-        r_old = r.copy()
-        u_old = u.copy()
+        v_drop = get_drop_force(r, u, l, R, R_d, E, sigma) * dt
+        F_neighb, c_neighb = get_neighb_force(r, u, l, R, R_d, E, sigma)
+        v_neighb = F_neighb * dt
+        v_propuls = u * v
+        dr_diff = np.sqrt(2.0 * D * dt) * np.random.standard_normal(r.shape)
+        r += (v_propuls + v_drop + v_neighb) * dt + dr_diff
+
+        numerics.torque(r, u, R_d, v_hydro_0, dt)
+
         u = diffusion.rot_diff(u, Dr, dt)
-        c_neighb = do_forces(r, u, l, R, R_d, r_old, u_old)
-
-        r_old = r.copy()
-        u_old = u.copy()
-        r = diffusion.diff(r, D, dt)
-        c_neighb += do_forces(r, u, l, R, R_d, r_old, u_old)
-
-        r_old = r.copy()
-        u_old = u.copy()
-        r += v * u * dt
-        c_neighb += do_forces(r, u, l, R, R_d, r_old, u_old)
-
-        r_old = r.copy()
-        u_old = u.copy()
-        u[c_neighb] = diffusion.rot_diff(u[c_neighb], Dr_c, dt)
-        c_neighb += do_forces(r, u, l, R, R_d, r_old, u_old)
+        if np.isfinite(Dr_c):
+            u[c_neighb] = diffusion.rot_diff(u[c_neighb], Dr_c, dt)
+        else:
+            u[c_neighb] = vector.sphere_pick(dim, c_neighb.sum())
 
         i += 1
         t += dt
-
         if out is not None and not i % every:
             out_fname = '%010f' % t
             np.savez(os.path.join(out, 'dyn', out_fname), r=r, u=u)
--- numerics.pyx
+++ numerics.pyx
@@ -1,20 +1,26 @@
 import numpy as np
 cimport numpy as np
 cimport cython
+from cmath import sqrt
+from libc.math cimport sqrt, exp, acos, sin, cos
+from ciabatta.cell_list import intro as cl_intro
+
+
+cdef double SMALL = 1e-10
 
 
 @cython.cdivision(True)
 @cython.boundscheck(False)
 def capsule_obstructed(np.ndarray[np.float_t, ndim=2] r,
                        np.ndarray[np.float_t, ndim=2] u,
-                       float l, float R, float R_d):
+                       double l, double R, double R_d):
     cdef:
         unsigned int i
-        float l_half = l / 2.0
-        float R_obs_sq = (R_d - R) ** 2
+        double l_half = l / 2.0
+        double R_obs_sq = (R_d - R) ** 2
         np.ndarray[np.float_t, ndim=1] r_obs = np.empty(3, dtype=np.float)
-
         np.ndarray[np.uint8_t, ndim=1] c = np.zeros(r.shape[0], dtype=np.uint8)
+
     for i in range(r.shape[0]):
         r_obs[0] = r[i, 0] + u[i, 0] * l_half
         r_obs[1] = r[i, 1] + u[i, 1] * l_half
@@ -30,3 +36,220 @@ def capsule_obstructed(np.ndarray[np.float_t, ndim=2] r,
             if r_obs[0] ** 2 + r_obs[1] ** 2 + r_obs[2] ** 2 > R_obs_sq:
                 c[i] = 1
     return np.array(c, dtype=np.bool)
+
+
+@cython.cdivision(True)
+@cython.boundscheck(False)
+def capsule_radial_distance_sq(np.ndarray[np.float_t, ndim=2] r,
+                               np.ndarray[np.float_t, ndim=2] u,
+                               double l, double R, double R_d):
+    cdef:
+        unsigned int i
+        double l_half = l / 2.0, r_rad_1_sq, r_rad_2_sq
+        np.ndarray[np.float_t, ndim=1] r_obs = np.empty(3)
+        np.ndarray[np.float_t, ndim=1] r_rad_sq = np.zeros(r.shape[0])
+
+    for i in range(r.shape[0]):
+        r_obs[0] = r[i, 0] + u[i, 0] * l_half
+        r_obs[1] = r[i, 1] + u[i, 1] * l_half
+        r_obs[2] = r[i, 2] + u[i, 2] * l_half
+
+        r_rad_1_sq = r_obs[0] ** 2 + r_obs[1] ** 2 + r_obs[2] ** 2
+
+        r_obs[0] = r[i, 0] - u[i, 0] * l_half
+        r_obs[1] = r[i, 1] - u[i, 1] * l_half
+        r_obs[2] = r[i, 2] - u[i, 2] * l_half
+
+        r_rad_2_sq = r_obs[0] ** 2 + r_obs[1] ** 2 + r_obs[2] ** 2
+
+        r_rad_sq[i] = max(r_rad_2_sq, r_rad_1_sq)
+    return r_rad_sq
+
+
+
+@cython.cdivision(True)
+@cython.boundscheck(False)
+cdef void line_segments_sep(
+        np.ndarray[np.float_t, ndim=1] s1,
+        np.ndarray[np.float_t, ndim=1] s2,
+        np.ndarray[np.float_t, ndim=1] w,
+        np.ndarray[np.float_t, ndim=1] sep):
+    '''
+    Returns the squared minimum separation distance between two line segments,
+    in 3 dimensions.
+
+    For two line segments `a` and `b` with end-points ar1, ar2, br1, br2.
+
+    Parameters
+    ----------
+    s1 = ar2 - ar1
+    s2 = br2 - br1
+    w = ar1 - br1
+    sep: array in which to store result
+
+    Returns
+    -------
+    sep: array
+        Squared separation distance between the two line segments.
+    '''
+    cdef:
+        double a = 0.0, b = 0.0, c = 0.0, d = 0.0, e = 0.0
+        double D, s1N, s1D, s1c, s2N, s2D, s2c, sep_sq = 0.0
+        unsigned int dim = s1.shape[0], i
+
+    for i in range(dim):
+        a += s1[i] ** 2
+        b += s1[i] * s2[i]
+        c += s2[i] ** 2
+        d += s1[i] * w[i]
+        e += s2[i] * w[i]
+
+    D = a * c - b ** 2
+
+    s1c = s1N = s1D = D
+    s2c = s2N = s2D = D
+
+    if D < SMALL:
+        s1N = 0.0
+        s1D = 1.0
+        s2N = e
+        s2D = c
+    else:
+        s1N = b * e - c * d
+        s2N = a * e - b * d
+        if s1N < 0.0:
+            s1N = 0.0
+            s2N = e
+            s2D = c
+        elif s1N > s1D:
+            s1N = s1D
+            s2N = e + b
+            s2D = c
+
+    if s2N < 0.0:
+        s2N = 0.0
+
+        if -d < 0.0:
+            s1N = 0.0
+        elif -d > a:
+            s1N = s1D
+        else:
+            s1N = -d
+            s1D = a
+    elif s2N > s2D:
+        s2N = s2D
+
+        if (-d + b) < 0.0:
+            s1N = 0.0
+        elif (-d + b ) > a:
+            s1N = s1D
+        else:
+            s1N = -d + b
+            s1D = a
+
+    if abs(s1N) < SMALL:
+        s1c = 0.0
+    else:
+        s1c = s1N / s1D
+    if abs(s2N) < SMALL:
+        s2c = 0.0
+    else:
+        s2c = s2N / s2D
+
+    for i in range(dim):
+        sep[i] = w[i] + (s1c * s1[i]) - (s2c * s2[i])
+
+
+cdef double force(double r, double w, double E, double sigma):
+    return E * (exp((r - w) / sigma) - 1.0)
+
+
+@cython.cdivision(True)
+@cython.boundscheck(False)
+def capsule_neighb_force(np.ndarray[np.float_t, ndim=2] r,
+                         np.ndarray[np.float_t, ndim=2] u,
+                         double l, double R, double L,
+                         double E, double sigma):
+    cdef:
+        unsigned int i, i_i2, i2, idim, n = r.shape[0], dim = r.shape[1]
+        double sep_sq_max = (2.0 * R) ** 2, l_half = l / 2.0
+        double mod_r_ij, mod_r_ij_sq
+        tuple dims = (dim,)
+        np.ndarray[np.uint8_t, ndim=1, cast=True] c = np.zeros((n,), dtype=np.uint8)
+        np.ndarray[int, ndim=2] inters
+        np.ndarray[int, ndim=1] intersi
+        np.ndarray[np.float_t, ndim=1] s1 = np.zeros(dims), s2 = np.zeros(dims)
+        np.ndarray[np.float_t, ndim=1] wd = np.zeros(dims), r1d = np.zeros(dims)
+        np.ndarray[np.float_t, ndim=1] r_ij = np.zeros([r.shape[1]])
+        np.ndarray[np.float_t, ndim=2] F = np.zeros([r.shape[0], r.shape[1]])
+
+    inters, intersi = cl_intro.get_inters(r, L, 2.0 * R + l)
+
+    for i in range(n):
+        if intersi[i] > 0:
+            for idim in range(dim):
+                s1[idim] = u[i, idim] * l
+                r1d[idim] = r[i, idim] - u[i, idim] * l_half
+        for i_i2 in range(intersi[i]):
+            i2 = inters[i, i_i2]
+            for idim in range(dim):
+                s2[idim] = u[i2, idim] * l
+                wd[idim] = r1d[idim] - (r[i2, idim] - u[i2, idim] * l_half)
+            line_segments_sep(s1, s2, wd, r_ij)
+            mod_r_ij_sq = r_ij[0] ** 2 + r_ij[1] ** 2 + r_ij[2] ** 2
+            if mod_r_ij_sq < sep_sq_max:
+                c[i] = 1
+                mod_r_ij = sqrt(mod_r_ij_sq)
+                for idim in range(dim):
+                    F[i, idim] += force(mod_r_ij, 2.0 * R, E, sigma) * r_ij[idim] / mod_r_ij
+    return F, np.array(c, dtype=np.bool)
+
+
+@cython.boundscheck(False)
+cdef double mag(np.ndarray[np.float_t, ndim=1] a):
+    return sqrt(a[0] ** 2 + a[1] ** 2 + a[2] ** 2)
+
+
+@cython.boundscheck(False)
+cdef double dot(np.ndarray[np.float_t, ndim=1] a,
+                np.ndarray[np.float_t, ndim=1] b):
+    return a[0] * b[0] + a[1] * b[1] + a[2] * b[2]
+
+
+@cython.cdivision(True)
+@cython.boundscheck(False)
+def torque(np.ndarray[np.float_t, ndim=2] r,
+           np.ndarray[np.float_t, ndim=2] u,
+           double R_d, double v_hydro_0, double dt):
+    cdef:
+        unsigned int i, idim, dim = r.shape[1]
+        double mag_u_o_perp, mag_u_n_perp, mag_u_n_par, th_o_perp, phi
+        double mag_r, mag_r_cp, mag_u_o_par
+        np.ndarray[np.float_t, ndim=1] rc = np.zeros(r.shape[1])
+        np.ndarray[np.float_t, ndim=1] r_cp = np.zeros(r.shape[1])
+        np.ndarray[np.float_t, ndim=1] u_perp = np.zeros(r.shape[1])
+        np.ndarray[np.float_t, ndim=1] u_par = np.zeros(r.shape[1])
+        np.ndarray[np.float_t, ndim=1] u_o_par = np.zeros(r.shape[1])
+
+    for i in range(r.shape[0]):
+        mag_r = mag(r[i])
+        for idim in range(dim):
+            rc[idim] = (r[i, idim] / mag_r) * R_d
+            r_cp[idim] = rc[idim] - r[i, idim]
+        mag_r_cp = mag(r_cp)
+        for idim in range(dim):
+            u_perp[idim] = r_cp[idim] / mag_r_cp
+
+        mag_u_o_perp = dot(u[i], u_perp)
+        for idim in range(dim):
+            u_o_par[idim] = u[i, idim] - mag_u_o_perp * u_perp[idim]
+        mag_u_o_par = mag(u_o_par)
+        for idim in range(dim):
+            u_par[idim] = u_o_par[idim] / mag_u_o_par
+        th_o_perp = acos(mag_u_o_perp)
+        phi = ((v_hydro_0 * dt) *
+               -2.0 * sin(-2.0 * th_o_perp) / mag(r_cp) ** 3)
+        mag_u_n_perp = cos(th_o_perp + phi)
+        mag_u_n_par = sqrt(1.0 - mag_u_n_perp ** 2)
+        for idim in range(dim):
+            u[i, idim] = mag_u_n_perp * u_perp[idim] + mag_u_n_par * u_par[idim]
